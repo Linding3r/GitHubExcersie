ffb6578675b52b6c430e82109e2d73651e939f91
Linding3r <thomasjuullindinger@yahoo.dk>
2022-02-25 13:17:45 +0100

    added askAboutAge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner sc = new Scanner(System.in);
+
+    public void greet(){
+        System.out.println("Hi! Please enter your name");
+        String name = sc.next();
+        System.out.println("Hi " + name + "!");
+    }
+    public int askAboutAge(){
+        int age;
+        System.out.println("What is your age?");
+        age = sc.nextInt();
+        return age;
+    }
 
     public static void main(String[] args) {
+        Main prg = new Main();
         System.out.println("It works");
+        prg.greet();
+        prg.askAboutAge();
     }
 }
